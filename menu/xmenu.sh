099d09ae6ecabe1f3070cc5f112f50146b712d80
Khoirul Bahra <107555389+MyMasWayVPN@users.noreply.github.com>
2023-02-20 01:26:30 +0700

    Update xmenu.sh

--- menu/xmenu.sh
+++ menu/xmenu.sh
@@ -24,32 +24,26 @@ do
 done < /root/expired.txt
 rm /root/expired.txt
 Name=$(curl -sS https://raw.githubusercontent.com/MyMasWayVPN/MyMasWayVPN.github.io/main/wkwkwkwk | grep $MYIP | awk '{print $2}')
-# Color Validation
-DF='\e[39m'
-Bold='\e[1m'
-Blink='\e[5m'
-yell='\e[33m'
-red='\e[31m'
-green='\e[32m'
-blue='\e[34m'
-PURPLE='\e[35m'
-cyan='\e[36m'
-Lred='\e[91m'
-Lgreen='\e[92m'
-Lyellow='\e[93m'
-NC='\e[0m'
-GREEN='\033[0;32m'
-ORANGE='\033[0;33m'
-LIGHT='\033[0;37m'
+#!/bin/bash
 clear
+echo -e "\e[36m╒════════════════════════════════════════════╕\033[0m"
+echo -e " \E[0;41;36m                 INFO SERVER                \E[0m"
+echo -e "\e[36m╘════════════════════════════════════════════╛\033[0m"
 uphours=`uptime -p | awk '{print $2,$3}' | cut -d , -f1`
 upminutes=`uptime -p | awk '{print $4,$5}' | cut -d , -f1`
 uptimecek=`uptime -p | awk '{print $6,$7}' | cut -d , -f1`
 cekup=`uptime -p | grep -ow "day"`
 IP=$(curl -s ipinfo.io/ip )
 ISPVPS=$( curl -s ipinfo.io/org )
-domen=$(/etc/xray/domain)
 #clear
+if [ "$cekup" = "day" ]; then
+echo -e "System Uptime   :  $uphours $upminutes $uptimecek"
+else
+echo -e "System Uptime   :  $uphours $upminutes"
+fi
+echo -e "IP-VPS          :  $IP"
+echo -e "ISP-VPS         :  $ISPVPS"
+
 # // Export Color & Information
 export RED='\033[0;31m'
 export GREEN='\033[0;32m'
@@ -69,7 +63,7 @@ else
 fi
 
 # // SSH Websocket Proxy
-xray=$(/etc/systemd/system/xray.service | grep Active | awk '{print $3}' | cut -d "(" -f2 | cut -d ")" -f1)
+xray=$(/etc/init.d/ssh xray | grep Active | awk '{print $3}' | cut -d "(" -f2 | cut -d ")" -f1)
 if [[ $xray == "running" ]]; then
     status_xray="${GREEN}ON${NC}"
 else
@@ -77,26 +71,28 @@ else
 fi
 
 # // SSH Websocket Proxy
-runn=$(/etc/systemd/system/runn.service | grep Active | awk '{print $3}' | cut -d "(" -f2 | cut -d ")" -f1)
+runn=$(/etc/init.d/ssh runn | grep Active | awk '{print $3}' | cut -d "(" -f2 | cut -d ")" -f1)
 if [[ $xray == "running" ]]; then
     status_runn="${GREEN}ON${NC}"
 else
     status_runn="${RED}OFF${NC}"
 fi
 
-clear 
 echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
 echo -e "\E[44;1;39m                     ⇱ INFORMASI VPS ⇲                        \E[0m"
 echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
-echo -e ""                                                                  
-echo -e "\e[33m Operating System     \e[0m:  "`hostnamectl | grep "Operating System" | cut -d ' ' -f5-`	
-echo -e "\e[33m Total Amount Of RAM  \e[0m:  $tram MB"
-echo -e "\e[33m System Uptime        \e[0m:  $uptime "
-echo -e "\e[33m Isp Name             \e[0m:  $ISP"
-echo -e "\e[33m Domain               \e[0m:  $domenn"	
-echo -e "\e[33m Ip Vps               \e[0m:  $IPVPS"	
-echo -e "\e[33m Client               \e[0m:  $Name"	
-echo -e "\e[33m Expired               \e[0m:  $Exp2"	
+echo -e ""
+echo -e "□ Sever Uptime        = $( uptime -p  | cut -d " " -f 2-10000 ) "
+echo -e "□ Current Time        = $( date -d "0 days" +"%d-%m-%Y | %X" )"
+echo -e "□ Current Domain      = $( cat /etc/xray/domain )"
+echo -e "□ Server IP           = ${IP}"
+echo -e "□ ISP           = ${ISPVPS}"
+echo -e "□ Clients Name        = $Name"
+echo -e "□ Exfired Script VPS   = $Exp2"
+echo -e "□ Time Reboot VPS     = 00:00 ${GREEN}( Jam 12 Malam )${NC}"
+echo -e "□ License Limit       = 3 VPS ${GREEN}( Persatu IP VPS )${NC}"
+echo -e "□ AutoScript By Dev   = XDRG ${GREEN}( MasWayVPN )${NC}"
+	
 echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
 echo -e "\E[44;1;39m                     ⇱ STATUS LAYANAN ⇲                       \E[0m"
 echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
